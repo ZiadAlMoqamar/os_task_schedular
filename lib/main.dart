f1d0e356984dbb02869057a89574b0667dd3c53b
Ziad Al-Moqamar <49427996+ZiadAlMoqamar@users.noreply.github.com>
2021-04-17 19:51:41 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'classes/process.dart';
 import 'components/chart.dart';
 import 'logic/fcfs_logic.dart' as fcfs_logic;
 import 'logic/sjf_logic.dart' as sjf_logic;
+import 'logic/srtf_logic.dart' as srtf_logic;
 
 void main() {
   runApp(MyApp());
@@ -36,18 +37,44 @@ class _MyHomePageState extends State<MyHomePage> {
   //   Process(processTitle: 'P3', startTime: 0, endTime: 5),
   //   Process(processTitle: 'P3', startTime: 0, endTime: 5),
   //   Process(processTitle: 'P3', startTime: 0, endTime: 5),
+  // srtf_logic.InputProcess(id: 1, burstTime: 7, arrivalTime: 0),
+  // srtf_logic.InputProcess(id: 2, burstTime: 5, arrivalTime: 1),
+  // srtf_logic.InputProcess(id: 3, burstTime: 3, arrivalTime: 2),
+  // srtf_logic.InputProcess(id: 4, burstTime: 1, arrivalTime: 3),
+  // srtf_logic.InputProcess(id: 5, burstTime: 2, arrivalTime: 4),
+  // srtf_logic.InputProcess(id: 6, burstTime: 1, arrivalTime: 5),
   // ];
 
+  // doctoer example
+// srtf_logic.InputProcess(id: 1, burstTime: 8, arrivalTime: 0),
+//       srtf_logic.InputProcess(id: 2, burstTime: 4, arrivalTime: 1),
+//       srtf_logic.InputProcess(id: 3, burstTime: 9, arrivalTime: 2),
+//       srtf_logic.InputProcess(id: 4, burstTime: 5, arrivalTime: 3),
+// net example
+// srtf_logic.InputProcess(id: 1, burstTime: 3, arrivalTime: 0),
+  // srtf_logic.InputProcess(id: 2, burstTime: 2, arrivalTime: 0),
+  // srtf_logic.InputProcess(id: 3, burstTime: 1, arrivalTime: 2),
+  // srtf_logic.InputProcess(id: 4, burstTime: 2, arrivalTime: 3),
+  // another example
+  // srtf_logic.InputProcess(id: 1, burstTime: 4, arrivalTime: 0),
+  //     srtf_logic.InputProcess(id: 2, burstTime: 5, arrivalTime: 1),
+  //     srtf_logic.InputProcess(id: 3, burstTime: 2, arrivalTime: 2),
+  //     srtf_logic.InputProcess(id: 4, burstTime: 1, arrivalTime: 3),
+  //     srtf_logic.InputProcess(id: 5, burstTime: 6, arrivalTime: 4),
+  //     srtf_logic.InputProcess(id: 6, burstTime: 3, arrivalTime: 5),
   @override
   Widget build(BuildContext context) {
     ///////////
     var input = [
-      fcfs_logic.InputProcess(id: 1, burstTime: 6),
-      fcfs_logic.InputProcess(id: 2, burstTime: 8),
-      fcfs_logic.InputProcess(id: 3, burstTime: 7),
-      fcfs_logic.InputProcess(id: 4, burstTime: 3),
+      srtf_logic.InputProcess(id: 1, burstTime: 7, arrivalTime: 0),
+      srtf_logic.InputProcess(id: 2, burstTime: 5, arrivalTime: 1),
+      srtf_logic.InputProcess(id: 3, burstTime: 3, arrivalTime: 2),
+      srtf_logic.InputProcess(id: 4, burstTime: 1, arrivalTime: 3),
+      srtf_logic.InputProcess(id: 5, burstTime: 2, arrivalTime: 4),
+      srtf_logic.InputProcess(id: 6, burstTime: 1, arrivalTime: 5),
     ];
-    var obj = sjf_logic.SJF(input);
+
+    var obj = srtf_logic.SRTF(input);
     print(obj.avgWaitingTime);
     /////////////////
     return Scaffold(
@@ -64,8 +91,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   procesess: obj.output.map((process) {
                 return Process(
                     processTitle: process.id.toString(),
-                    startTime: process.waitingTime.toInt(),
-                    endTime: process.waitingTime.toInt()+process.burstTime.toInt());
+                    startTime: 0,
+                    endTime: process.endBurstTime.toInt());
               }).toList()),
             ),
             Text("AVG Waiting time: " + obj.avgWaitingTime.toString())
