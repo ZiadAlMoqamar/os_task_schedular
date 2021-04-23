414ff7891d733ec5551bad11cfa32818463091c0
Ziad Al-Moqamar <49427996+ZiadAlMoqamar@users.noreply.github.com>
2021-04-23 05:38:57 +0200

    added idle state

--- lib/logic/srtf_logic.dart
+++ lib/logic/srtf_logic.dart
@@ -6,10 +6,11 @@ class InputProcess {
   int id;
   int burstTime;
   int arrivalTime;
-  InputProcess(
-      {this.id = 0,
-      this.burstTime = 0,
-      this.arrivalTime = 0,});
+  InputProcess({
+    this.id = 0,
+    this.burstTime = 0,
+    this.arrivalTime = 0,
+  });
 }
 
 class AvgOutputProcess {
@@ -60,10 +61,10 @@ class SRTF {
     input.forEach((element) {
       burstTimeList.add(element.burstTime);
     });
-    // var originalInput = input;
+
     var minArrivalTime = input.first.arrivalTime;
-    var currentTime = minArrivalTime;
-    var currentPid = 0;
+    var currentTime = 0;
+    var currentPid = -1;
     int currentIndex;
     //used to incremant on process endtime
     var lastEndTime = 0;
@@ -76,16 +77,31 @@ class SRTF {
     while (currentTime <= maxArrivalTime) {
       //=-1 to avoid the second if scope
       int nextIndex = -1;
-      //maybe useless but sort in obj passes by ref. !!
-      var funcInput = input;
+
       //used to find next process to compare its remaining burst time
-      nextIndex = minBurstProcessIndex(funcInput, currentTime);
+      nextIndex = minBurstProcessIndex(input, currentTime);
       input.sort((a, b) => a.arrivalTime.compareTo(b.arrivalTime));
+      //for idle state
+      if (nextIndex == -1 && currentFinishedBurst == 0) {
+        for (int m = 0; m < input.length; m++) {
+          int mBurstTime = input[m].burstTime;
+          int mArrivalTime = input[m].arrivalTime;
+          if ((mBurstTime != 0) && (mArrivalTime > currentTime)) {
+            output.add(OutputProcess(id: -1, endBurstTime: mArrivalTime));
+            currentTime = mArrivalTime;
+            lastEndTime = mArrivalTime;
+            currentFinishedBurst = 0;
+            nextIndex = minBurstProcessIndex(input, currentTime);
+            currentIndex = nextIndex;
+            currentPid = input[currentIndex].id;
+            break;
+          }
+        }
+      }
       //this scope used for the first iteration
       if (currentTime == minArrivalTime) {
         currentPid = input[nextIndex].id;
-        // print(
-        //     "current index $currentIndex will change to nextIndex $nextIndex");
+
         currentIndex = nextIndex;
         currentFinishedBurst = 0;
       }
@@ -96,30 +112,23 @@ class SRTF {
           //adding outputProcess
           output.add(OutputProcess(id: currentPid, endBurstTime: currentTime));
           burstTimeList[currentIndex] -= currentFinishedBurst;
-          // print("current index $currentIndex of pid ${input[currentIndex].id}");
-          // print("current index $currentIndex & pid of 0 ${input[0].id}");
-          // print("currentime " + currentTime.toString());
-          // print("pid added " + currentPid.toString());
-          // print(
-          //     "zz currentFinishedBurst =$currentFinishedBurst of pid $currentPid");
           lastEndTime = currentTime;
           currentPid = input[nextIndex].id;
-          // print(
-          //     "current index $currentIndex will change to nextIndex $nextIndex");
+
           currentIndex = nextIndex;
           currentFinishedBurst = 0;
         }
+      } else if (currentPid == -1 && currentIndex == null) {
+        output.add(OutputProcess(id: currentPid, endBurstTime: minArrivalTime));
+        currentTime = minArrivalTime;
+        lastEndTime = minArrivalTime;
+        currentFinishedBurst = 0;
       }
-      // print("current time =$currentTime will be incremented now");
-      // print(
-      //     "burst time of current index $currentIndex will be decremented now of pid ${input[currentIndex].id}");
-      // print(
-      //     "xx currentFinishedBurst =$currentFinishedBurst of pid $currentPid");
       //this scope to stop iterating on finished process
-      if (input[currentIndex].burstTime <= 0) {
-        // print("xx input[currentindx].burst =${input[currentIndex].burstTime} of pid $currentPid");
+      bool finished = false;
+      if (currentIndex != null && input[currentIndex].burstTime <= 0) {
         var x = minBurstProcessIndexLessthanCurrentTime(input, currentTime);
-        //print( "zz currentFinishedBurst =$currentFinishedBurst of pid $currentPid");
+
         lastEndTime += currentFinishedBurst;
         //workaround for the 0 currentFinishedBurst
         if (currentFinishedBurst == 0) {
@@ -127,43 +136,42 @@ class SRTF {
         }
         output.add(OutputProcess(id: currentPid, endBurstTime: lastEndTime));
         burstTimeList[currentIndex] -= currentFinishedBurst;
-        // print("hoho pid added $currentPid");
+        finished = true;
 
-        currentPid = input[x].id;
-        //print("hoho current index will be changed from $currentIndex to $x");
-        currentIndex = x;
+        if (x != -1) {
+          currentPid = input[x].id;
+
+          currentIndex = x;
+          currentFinishedBurst = 0;
+          finished = false;
+        }
         currentFinishedBurst = 0;
       }
-      //print( "yy before decrement input[currentindx].burst =${input[currentIndex].burstTime} of pid $currentPid");
-      currentFinishedBurst++;
-      input[currentIndex].burstTime--;
-      currentTime++;
+
+      if (currentPid != -1 && finished == false) {
+        currentFinishedBurst++;
+        input[currentIndex].burstTime--;
+        currentTime++;
+      }
     }
     //after reachimg maxArrivalTime
 
     //if the current process is the shortest remainig job of all the rest
     if (currentPid == minBurstProcessid(input)) {
-      //print("ana aho bursttime ${input[currentIndex].burstTime}");
-
       lastEndTime = currentTime + input[currentIndex].burstTime;
-      // lastEndTime+= input[currentIndex].burstTime;
+
       output.add(OutputProcess(id: currentPid, endBurstTime: lastEndTime));
-      //print("ana aho pid added $currentPid");
-      //print("ana aho lastEndTime $lastEndTime");
 
       //remove finished jobs from input
       input.remove(input[currentIndex]);
     }
     //calculating the remainig burst time for the remaining jobs
     else {
-      //var burstTimeList=[7,5,2,1,2,1];
-      // int finishedBurst =
-      //     originalInput[currentIndex].burstTime - input[currentIndex].burstTime;
       int finishedBurst =
           burstTimeList[currentIndex] - input[currentIndex].burstTime;
       lastEndTime += finishedBurst;
       output.add(OutputProcess(id: currentPid, endBurstTime: lastEndTime));
-      //print("while loop exit added pid is $currentPid");
+
       var removalIndex = input.indexWhere((element) => element.burstTime <= 0);
       input.remove(input[removalIndex]);
     }
@@ -171,25 +179,22 @@ class SRTF {
     for (int i = 0; i < input.length; i++) {
       if (input[i].burstTime <= 0) {
         input.remove(input[i]);
+        i = -1;
       }
     }
-    //print("lastend " + lastEndTime.toString());
-    //print("sjf currentime " + currentTime.toString());
+
     //sorting the remaining jobs according to burst time as simple SJF
     input.sort((a, b) => a.burstTime.compareTo(b.burstTime));
     for (var i = 0; i < input.length; i++) {
-      //print("pid = ${input[i].id} with bursttime = ${input[i].burstTime}");
-      //print("ahaaa lastEndTime = $lastEndTime");
       output.add(OutputProcess(
           id: input[i].id, endBurstTime: lastEndTime + input[i].burstTime));
-      //print("pid added ${input[i].id}");
+
       lastEndTime = lastEndTime + input[i].burstTime;
     }
     return output;
   }
 
   int minBurstProcessid(List<InputProcess> input) {
-    //print("hi minBurstProcessid is used");
     var x;
     input.sort((a, b) => a.burstTime.compareTo(b.burstTime));
     for (var i = 0; i < input.length; i++) {
@@ -209,18 +214,18 @@ class SRTF {
     for (int i = 0; i < input.length; i++) {
       if (input[i].arrivalTime == currentTime) {
         pid = input[i].id;
-        //print("at currenttime $currentTime minBurst pid is " + input[i].id.toString());
+
         input.sort((a, b) => a.arrivalTime.compareTo(b.arrivalTime));
-        //search for index of pid in a sorted to arrival time input
+
         index = input.indexWhere((element) => element.id == pid);
-        //print("right index is $index");
-        //print("minBurstpid after search ${input[index].id}");
+
         break;
       }
     }
     return index;
   }
 
+  //get process still have burst with arrival time less than current time
   int minBurstProcessIndexLessthanCurrentTime(
       List<InputProcess> input, int currentTime) {
     var index = -1;
@@ -229,12 +234,11 @@ class SRTF {
     for (int i = 0; i < input.length; i++) {
       if (input[i].arrivalTime <= currentTime && input[i].burstTime > 0) {
         pid = input[i].id;
-        //print("at currenttime $currentTime minBurst pid is " + input[i].id.toString());
+
         input.sort((a, b) => a.arrivalTime.compareTo(b.arrivalTime));
         //search for index of pid in a sorted to arrival time input
         index = input.indexWhere((element) => element.id == pid);
-        //print("right index is $index");
-        //print("minBurstpid after search ${input[index].id}");
+
         break;
       }
     }
